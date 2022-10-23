21df671b35e1d42c7f51bc3bdd3b7b576bc44572
Kenny <101839505+khui0@users.noreply.github.com>
2022-10-23 10:07:08 -0400

    Fix time calculations

--- main.js
+++ main.js
@@ -49,15 +49,15 @@ else {
 function timeBetween(date) {
     let target = new Date(date).getTime();
     let milliseconds = target - Date.now();
-    let seconds = Math.floor(milliseconds / 1000);
-    let minutes = Math.floor(seconds / 60);
-    let hours = Math.floor(minutes / 60);
-    let days = Math.floor(hours / 24);
+    let seconds = Math.floor(Math.abs(milliseconds / 1000));
+    let minutes = Math.floor(Math.abs(seconds / 60));
+    let hours = Math.floor(Math.abs(minutes / 60));
+    let days = Math.floor(Math.abs(hours / 24));
     return {
-        days: Math.abs(days),
-        hours: Math.abs(hours % 24),
-        minutes: Math.abs(minutes % 60),
-        seconds: Math.abs(seconds % 60),
+        days: days,
+        hours: hours % 24,
+        minutes: minutes % 60,
+        seconds: seconds % 60,
         passed: milliseconds < 0
     }
 }
